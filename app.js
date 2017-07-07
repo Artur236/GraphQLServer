4126f444f5f54c47ec3f87e06f228ec3c4162a52
Arthur Poliakov <poliakov.arthur@gmail.com>
2017-07-07 13:41:24 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import mongoose from 'mongoose';
+
+import schema from './graphql';
+
+var app = express();
+
+// GraphqQL server route
+app.use('/graphql', graphqlHTTP(req => ({schema, pretty: true, graphiql: true})));
+
+// Connect mongo database
+mongoose.connect('mongodb://localhost:27017/graphql');
+
+// start server
+var server = app.listen(3030, () => {
+    console.log('Listening at port', server.address().port);
+});
